9cbd2caf07cdf4bd858c504ab20e4d425ddd4a62
Nikhil Thota <nt92@ufl.edu>
2016-02-16 20:23:09 -0500

    Added login view

--- NikhilGram/NikhilGram/AppDelegate.swift
+++ NikhilGram/NikhilGram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "INSTAGRAM_NIKHIL"
+                configuration.clientKey = "codepath"
+                configuration.server = "https://instagram-nikhil.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
